401a8672a0de380701097913a9e9c691cc778e96
ElviraS <elvirasharf@gmail.com>
2019-02-22 15:28:13 -0800

    Added testUpdateStudentNamePositive unit test.

--- src/main/java/UnitTestNG.java
+++ src/main/java/UnitTestNG.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class UnitTestNG {
     @Test
     public void testAddStudentPositive() {
@@ -20,12 +19,30 @@ public class UnitTestNG {
         StudentManagement management = new StudentManagement();
         Student student1 = new Student("elvira");
         Assert.assertEquals(management.addStudent(student1), true);
-        Assert.assertEquals(management.addStudent(student1), false);
+        boolean actual = management.addStudent(student1);
+        boolean expected = false;
+        Assert.assertEquals(actual, expected);
+    }
+
+    // expect: is what you expect to get back. Hardcoded.
+    // actual: result from your method.
+    @Test
+    public void testUpdateStudentNamePositive() {
+        StudentManagement management = new StudentManagement();
+        Student student1 = new Student("elvira");
+        management.addStudent(student1);
+        management.updateStudentName("elvira", "ela");
+        management.printStudents();
+        ArrayList<Student> listOfStudents = management.getList();
+
+        // 1. get list.
+        // 2. iterate over list.
+        // 3. find our name
+
+        String actual = listOfStudents.get(0).name;
+        String expected = "ela";
+        Assert.assertEquals(actual, expected);
+
     }
 }
-
-
-
-
-
 
